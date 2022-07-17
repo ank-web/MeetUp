bb68a1aa594c7349fb34860b1cae804b781098f4
ankit <59723644+ank-web@users.noreply.github.com>
2022-07-17 13:19:39 +0530

    Added Store

---
+++ src/components/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Codeial</div>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import { configureStore } from "./store";
+
+const store = configureStore();
+console.log('store',store.getState());
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
