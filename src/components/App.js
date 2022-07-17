2c0068542cc065c17103836d72910e4dfd5fad96
ankit <59723644+ank-web@users.noreply.github.com>
2022-07-17 20:10:14 +0530

    ADD Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,86 @@
-function App() {
-  return <div>Codeial</div>;
+import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/posts";
+import { PostsList } from "./index";
+import PropTypes from "prop-types";
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://th.bing.com/th/id/OIP.tkezL3Y1TkpZM3IxjGfpkwHaHa?pid=ImgDet&w=1596&h=1596&rs=1"
+              alt="logo"
+              width="50px"
+              height="50px"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://image.flaticon.com/icons/svg/483/483356.svg"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  />
+                  <span>Udgam</span>
+                </li>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="user-dp"
+                  />
+                  <span>Udgam</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                alt="user-dp"
+                id="user-dp"
+              />
+              <span>Udgam</span>
+            </div>
+            <div className="nav-links">
+              <ul>
+                <li>Log in</li>
+                <li>Log out</li>
+                <li>Register</li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <PostsList posts={posts} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { configureStore } from "./store";
+import { Provider } from "react-redux";
+import './index.css';
 
 const store = configureStore();
-console.log('store',store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store = {store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
